68d62d66259dde5af922eb983858bf52a0d502e0
Sebastian Marchant <sebastian.marchant1@cloud.uautonoma.cl>
2024-10-16 22:53:50 -0300

    pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import axios from "axios";
+import './App.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaDeCompras from './components/listadecompras';
+import CompraCreate from './components/compraCreate';
+import CompraEdit from './components/compraEdit';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [compras, setCompras] = useState([]); // array donde se guardan las compras
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<ListaDeCompras compras={compras} setCompras={setCompras} />} />
+                    <Route path='/compra/create' element={<CompraCreate compras={compras} setCompras={setCompras} />} />
+                    <Route path='/compra/edit/:id' element={<CompraEdit compras={compras} setCompras={setCompras} />} />
+                </Routes>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
-export default App
+export default App;
